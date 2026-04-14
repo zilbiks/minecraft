43847ec96349d7ba794e2cf9cec9dd10a0153b18
zilbiks <thelinskyy.trade@gmail.com>
2026-04-14 14:24:34 +0300

    Switch problem source to local CSV dataset

--- code_trainer/config.py
+++ code_trainer/config.py
@@ -1,12 +1,6 @@
-LEETCODE_API_BASE = "https://alfa-leetcode-api.onrender.com"
+DATASET_PATH = "questions_dataset.csv"
 
 DEFAULT_DIFFICULTY = "Easy"
 DEFAULT_PROBLEMS_LIMIT = 15
 
 DEFAULT_EXAMPLES_TO_TEST = 3
-
-DIFFICULTY_TO_API = {
-    "Easy": "EASY",
-    "Medium": "MEDIUM",
-    "Hard": "HARD",
-}
